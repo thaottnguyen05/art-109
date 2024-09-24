e7f1c57c7fd329ddcaa35449a07dd1b30a7bedc5
thaottnguyen05 <thaonguyeen09@gmail.com>
2024-09-24 13:43:07 -0700

    java dom tut

--- class-exercises/dom/js/main.js
+++ class-exercises/dom/js/main.js
@@ -1,20 +1,29 @@
 
-console.log("hello hello");
-
-
-let pageTitle = document.querySelector("#page-title")
-
-// Javascript Timeout changes h1 title after 3 seconds
-setTimeout(function(){
-    pageTitle.style.color = "pink";
-    // console.log("timeout worked!")
-} , 3000)
-
-let header = document.querySelector("header")
-let body = document.querySelector("body")
-// Click event on header changes background color
-header.onclick = function() {
-    // console.log("clicked header");
-    body.style.backgroundColor = "black";
-}
-
+document.querySelector("#image-0").addEventListener("click", function(){
+    document.querySelector("#image-0").style.visibility = "hidden"
+})
+ 
+document.querySelector("#image-1").addEventListener("click", function(){
+    document.querySelector("#image-1").style.visibility = "hidden"
+})
+ 
+document.querySelector("#image-2").addEventListener("click", function(){
+    document.querySelector("#image-2").style.visibility = "hidden"
+})
+ 
+document.querySelector("#image-3").addEventListener("click", function(){
+    document.querySelector("#image-3").style.visibility = "hidden"
+})
+ 
+document.querySelector("#image-4").addEventListener("click", function(){
+    document.querySelector("#image-4").style.visibility = "hidden"
+})
+ 
+document.querySelector("#image-5").addEventListener("click", function(){
+    document.querySelector("#image-5").style.visibility = "hidden"
+})
+ 
+document.querySelector("#image-6").addEventListener("click", function(){
+    document.querySelector("#image-6").style.visibility = "hidden"
+})
+ 
